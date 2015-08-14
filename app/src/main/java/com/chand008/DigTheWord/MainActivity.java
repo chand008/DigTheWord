0dbd267a50bf54fbcc422e036addc9ff0fc31aef
chand008 <chandhnik08@gmail.com>
2015-08-14 15:20:07 -0700

    uploading latest apk

--- app/src/main/java/com/chand008/DigTheWord/MainActivity.java
+++ app/src/main/java/com/chand008/DigTheWord/MainActivity.java
@@ -18,7 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android .view.View;
+import android.view.View;
 import android.content.Intent;
 
 import com.chand008.DigTheWord.R;
